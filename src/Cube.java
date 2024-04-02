98eb630136aac02d9bafe1dbe7e473b2e304e5c6
dkostadinov <d78mdd@gmail.com>
2024-04-02 19:54:41 +0300

    - initial frame 1 - output cube contents

--- src/Cube.java
+++ src/Cube.java
@@ -1,3 +1,146 @@
+import java.util.Arrays;
+
 public class Cube {
+
+    private int[][] sideRight = new int[3][3];
+    private int[][] sideLeft = new int[3][3];
+    private int[][] sideUp = new int[3][3];
+    private int[][] sideDown = new int[3][3];
+    private int[][] sideFront = new int[3][3];
+    private int[][] sideBack = new int[3][3];
+
+
+    public Cube(int[][] sideRight, int[][] sideLeft, int[][] sideUp, int[][] sideDown, int[][] sideFront, int[][] sideBack) {
+        this.sideRight = sideRight;
+        this.sideLeft = sideLeft;
+        this.sideUp = sideUp;
+        this.sideDown = sideDown;
+        this.sideFront = sideFront;
+        this.sideBack = sideBack;
+    }
+
+
+
+
+    public void moveLayer(char layer) {
+    }
+    public void rotateCube(char axis) {
+    }
+
+
+
+
+    public void moveLayerTop() {
+        moveLayer('U');
+    }
+
+    public void moveLayerMiddleHorizontal() {
+        moveLayer('U');
+        moveLayer('D');
+    }
+
+    public void moveLayerBottom() {
+        moveLayer('D');
+    }
+
+
+
+    public void moveLayerFront() {
+        moveLayer('F');
+    }
+
+    public void moveLayerMiddleVertical() {
+        moveLayer('F');
+        moveLayer('B');
+    }
+
+    public void moveLayerBack() {
+        moveLayer('B');
+    }
+
+
+
+    public void moveLayerLeft() {
+        moveLayer('L');
+    }
+
+    public void moveLayerVertical2() {
+        moveLayer('L');
+        moveLayer('R');
+    }
+
+    public void moveLayerRight() {
+        moveLayer('R');
+    }
+
+
+
+    public void rotateCubeX() {
+        rotateCube('x');
+    }
+
+    public void rotateCubeY() {
+        rotateCube('y');
+    }
+
+    public void rotateCubeZ() {
+        rotateCube('z');
+    }
+
+
+    public void outputSide(String side) {
+        switch (side) {
+            case "right":
+                System.out.println(Arrays.deepToString(sideRight));
+                break;
+            case "left":
+                System.out.println(Arrays.deepToString(sideLeft));
+                break;
+            case "up":
+                System.out.println(Arrays.deepToString(sideUp));
+                break;
+            case "down":
+                System.out.println(Arrays.deepToString(sideDown));
+                break;
+            case "front":
+                System.out.println(Arrays.deepToString(sideFront));
+                break;
+            case "back":
+                System.out.println(Arrays.deepToString(sideBack));
+                break;
+        }
+
+    }
+
+
+    public void outputCurrentState() {
+        outputSide("right");
+        outputSide("left");
+        outputSide("up");
+        outputSide("down");
+        outputSide("front");
+        outputSide("back");
+    }
+
+
+
+    public void solveLayer1() {
+
+    }
+
+    public void solveLayer2() {
+
+    }
+
+    public void solveLayer3() {
+
+    }
+
+    public void solve() {
+        solveLayer1();
+        solveLayer2();
+        solveLayer3();
+    }
+
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[][] side1 = {{1, 2, 3}, {1, 2, 3}, {1, 2, 3}};
+        int[][] side2 = {{1, 2, 3}, {1, 2, 3}, {1, 2, 3}};
+        int[][] side3 = {{1, 2, 3}, {1, 2, 3}, {1, 2, 3}};
+        int[][] side4 = {{1, 2, 3}, {1, 2, 3}, {1, 2, 3}};
+        int[][] side5 = {{1, 2, 3}, {1, 2, 3}, {1, 2, 3}};
+        int[][] side6 = {{1, 2, 3}, {1, 2, 3}, {1, 2, 3}};
+
+        Cube cube1 = new Cube(side1, side2, side3, side4, side5, side6);
+
+        cube1.outputCurrentState();
+
     }
 }
